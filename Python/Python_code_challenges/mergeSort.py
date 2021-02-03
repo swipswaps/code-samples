628cbba651bc2e48adafb16e9f049a9c7ac48015
owner <owner@debian10>
2021-02-03 01:36:10 -0800

    Commit 02-02-21

---
+++ Python/Python_code_challenges/mergeSort.py
@@ -0,0 +1,27 @@
+""" mergeSort
+Implement the Merge Sort algorithm to sort arrays. The input is a one-dimensional array
+of integers.
+"""
+
+
+def mergeSort(arr):
+    recurMS(0, len(arr), arr)
+    return arr
+
+def recurMS(start, end, arr):
+    if end - start < 2:
+        return
+    mid = start + (end-start) // 2
+    recurMS(start, mid, arr)
+    recurMS(mid, end,  arr)
+    temp = []
+    right = mid
+    for left in range(start, mid):
+        while right < end and arr[right] < arr[left]:
+            temp.append(arr[right])
+            right += 1
+        temp.append(arr[left])
+    arr[start:start+len(temp)] = temp
+
+
+print(mergeSort([4, 3, 7, 2, 5, 8, 27, 0, 2, 2])) # [0, 2, 2, 2, 3, 4, 5, 7, 8, 27]
